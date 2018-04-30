4dee406bd04cc23fa5c4104a16a0acccc5488664
Codevski <1435321+codevski@users.noreply.github.com>
2018-04-30 16:22:54 +1000

    Added

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,89 @@
+import './vendor.ts';
+
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Ng2Webstorage, LocalStorageService, SessionStorageService  } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { FeedmeSharedModule, UserRouteAccessService } from './shared';
+import { FeedmeAppRoutingModule} from './app-routing.module';
+import { FeedmeHomeModule } from './home/home.module';
+import { FeedmeAdminModule } from './admin/admin.module';
+import { FeedmeAccountModule } from './account/account.module';
+import { FeedmeEntityModule } from './entities/entity.module';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FeedmeAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        FeedmeSharedModule,
+        FeedmeHomeModule,
+        FeedmeAdminModule,
+        FeedmeAccountModule,
+        FeedmeEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        PaginationConfig,
+        UserRouteAccessService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+            deps: [
+                LocalStorageService,
+                SessionStorageService
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [
+                JhiEventManager
+            ]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [
+                Injector
+            ]
+        }
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class FeedmeAppModule {}
